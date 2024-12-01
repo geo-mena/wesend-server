caaabb0a1d14e2ec82563355f4ec23aef57d18d1
Geovanny Mena <geovannymenavc@gmail.com>
2024-12-01 17:12:06 -0500

    ✨ enhance SendEmailNotificationJob to handle expiration date and add error logging for email sending

--- app/Jobs/SendEmailNotificationJob.php
+++ app/Jobs/SendEmailNotificationJob.php
@@ -3,11 +3,14 @@
 namespace App\Jobs;
 
 use App\Models\Transfer;
+use Carbon\Carbon;
+use Exception;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Log;
 use Resend\Laravel\Facades\Resend;
 
 class SendEmailNotificationJob implements ShouldQueue
@@ -19,18 +22,25 @@ class SendEmailNotificationJob implements ShouldQueue
     public function __construct(Transfer $transfer)
     {
         $this->transfer = $transfer;
+
+        $this->transfer->expires_at = Carbon::parse($this->transfer->expires_at);
     }
 
     public function handle()
     {
-        // Enviar email usando Resend
-        Resend::emails()->send([
-            'from' => 'notifications@yourdomain.com',
-            'to' => $this->transfer->recipient_email,
-            'subject' => 'Archivos compartidos contigo',
-            'html' => view('emails.transfer', [
-                'transfer' => $this->transfer
-            ])->render()
-        ]);
+        try {
+            // Enviar email usando Resend
+            Resend::emails()->send([
+                'from' => 'noreply@signature-server.online',
+                'to' => $this->transfer->recipient_email,
+                'subject' => 'Archivos compartidos contigo',
+                'html' => view('emails.transfer', [
+                    'transfer' => $this->transfer
+                ])->render()
+            ]);
+        } catch (Exception $e) {
+            Log::error('Resend error: ' . $e->getMessage());
+            throw $e;
+        }
     }
 }
