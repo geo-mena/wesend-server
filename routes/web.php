ff1e6f8e5b20b308480e89d69dc1ad93ef022f88
Geovanny Mena <geovannymenavc@gmail.com>
2024-12-01 12:10:16 -0500

    🔀 remove deprecated file upload and transfer routes from API v1

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,5 @@
 <?php
 
-use App\Http\Controllers\File\FileController;
-use App\Http\Controllers\File\TransferController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,12 +15,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
-
-Route::prefix('v1')->group(function () {
-    Route::post('upload/chunk', [FileController::class, 'upload']);
-    Route::post('upload/finalize', [FileController::class, 'finalize']);
-    Route::post('transfer/email', [TransferController::class, 'createEmailTransfer']);
-    Route::post('transfer/link', [TransferController::class, 'createLinkTransfer']);
-    Route::get('download/{token}', [TransferController::class, 'download'])->name('download');
 });
