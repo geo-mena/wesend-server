86d4421f17f5c1725da258173c3e7eab614a3dfd
Geovanny Mena <geovannymenavc@gmail.com>
2024-11-30 21:33:28 -0500

    📦️ add File and Transfer models with relationships

---
+++ app/Models/Transfer.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Transfer extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'type',
+        'message',
+        'password',
+        'sender_email',
+        'recipient_email',
+        'download_token',
+        'expires_at',
+    ];
+
+    public function files()
+    {
+        return $this->belongsToMany(File::class, 'transfer_files');
+    }
+}
