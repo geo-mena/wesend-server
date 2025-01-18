ad75e7527020cb976f9f3df9c142b8a529a25bb7
Geovanny Mena <geovannymenavc@gmail.com>
2025-01-18 11:04:15 -0500

    🌱 update route to point to home.welcome view

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home.welcome');
 });
 
 Route::prefix('v1')->group(function () {
