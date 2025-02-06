b80aa9d8a971123caf1332e4a4a73626fef8aa38
Geovanny Mena <geovannymenavc@gmail.com>
2025-02-05 20:41:52 -0500

    🌱 implement temporary database creation and retrieval functionality

---
+++ database/migrations/2025_02_05_183935_create_temporary_databases_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('temporary_databases', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->text('connection_url');
+            $table->string('branch_id');
+            $table->timestamp('expires_at');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('temporary_databases');
+    }
+};
